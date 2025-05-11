ea061203537ca0939bdf9e9a5122f3a05bd34914
deepanshu <deepanshukamlesh@gamil.com>
2025-05-11 20:02:44 +0530

    initial setup and integration of sequelize ORM

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,13 @@ type ServerConfig = {
     PORT: number
 }
 
+type DBConfig = {
+    DB_HOST: string,
+    DB_USER: string,
+    DB_PASSWORD: string,
+    DB_NAME: string
+}
+
 function loadEnv() {
     dotenv.config();
     console.log(`Environment variables loaded`);
@@ -14,4 +21,11 @@ loadEnv();
 
 export const serverConfig: ServerConfig = {
     PORT: Number(process.env.PORT) || 3001
+};
+
+export const dbConfig: DBConfig = {
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'root',
+    DB_NAME: process.env.DB_NAME || 'test_db'
 };
