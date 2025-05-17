dd83c9e395170285a8336889b3e25de9c50a9e3c
deepanshu <deepanshukamlesh@gamil.com>
2025-05-17 20:20:21 +0530

    Implemented get all hotels,create hotel and get hotel by id apis and integrated http status code package

---
+++ src/controllers/hotel.controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import { createHotelService, getAllHotelsService, getHotelByIdService } from "../services/hotel.service";
+import { StatusCodes } from "http-status-codes";
+
+export async function createHotelHandler(req: Request, res: Response, next: NextFunction) {
+    // 1. Call the service layer
+
+    const hotelResponse = await createHotelService(req.body);
+
+    // 2. Send the response
+
+    res.status(StatusCodes.CREATED).json({
+        message: "Hotel created successfully",
+        data: hotelResponse,
+        success: true,
+    })
+}
+
+export async function getHotelByIdHandler(req: Request, res: Response, next: NextFunction) {
+    // 1. Call the service layer
+
+    const hotelResponse = await getHotelByIdService(Number(req.params.id));
+
+    // 2. Send the response
+
+    res.status(StatusCodes.OK).json({
+        message: "Hotel found successfully",
+        data: hotelResponse,
+        success: true,
+    })
+}
+
+export async function getAllHotelsHandler(req: Request, res: Response, next: NextFunction) {
+
+    // 1. Call the service layer
+
+    const hotelsResponse = await getAllHotelsService();
+
+    // 2. Send the response
+    res.status(StatusCodes.OK).json({
+        message: "Hotels found successfully",
+        data: hotelsResponse,
+        success: true,
+    });
+
+}
+
+export async function deleteHotelHandler(req: Request, res: Response, next: NextFunction) {
+
+    res.status(StatusCodes.NOT_IMPLEMENTED);
+    
+}
+
+export async function updateHotelHandler(req: Request, res: Response, next: NextFunction) {
+
+    res.status(StatusCodes.NOT_IMPLEMENTED);
+    
+}
---
+++ src/routers/v1/hotel.router.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { createHotelHandler, getAllHotelsHandler, getHotelByIdHandler } from '../../controllers/hotel.controller';
+import { validateRequestBody } from '../../validators';
+import { hotelSchema } from '../../validators/hotel.validator';
+
+const hotelRouter = express.Router();
+
+hotelRouter.post(
+    '/', 
+    validateRequestBody(hotelSchema),
+    createHotelHandler); 
+
+hotelRouter.get('/:id', getHotelByIdHandler); 
+
+hotelRouter.get('/', getAllHotelsHandler);
+
+export default hotelRouter; 
